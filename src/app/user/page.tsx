d48418174c73231ccf1ceee9c5c998c7ec463a19
温漂平 <wenpiaoping@192.168.0.101>
2024-02-07 00:52:22 +0800

    xifu

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,28 +2,20 @@
 import Link from "next/link"
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
+import { BackButton, useWebApp } from '@vkruglikov/react-telegram-web-app';
 import {Image,Divider } from 'react-vant';
 
 export default function User() {
-    const router = useRouter();
-
-  useEffect(() => {
-    try {
-         // @ts-ignore
-      const appContext = window?.Telegram.WebApp;
-
-      // 设置标题栏样式
-      appContext.setTitleBarStyle('个人中心');
-
-      // 添加返回按钮，并设置点击事件处理函数
-      appContext.setBackButton(() => {
-        router.back();
-      });
-    } catch (err) {}
-  }, []);
+    const { back } = useRouter();
+    const { initWebAppData } = useWebApp();
+    useEffect(() => {
+        // 获取用户信息
+        const user = initWebAppData.user;
+        console.log(user);
+      }, []);
   return (
     <div className='container p-4 pt-6'>
+        <BackButton onClick={back} />
         <div className="flex items-center justify-start w-full h-24">
         <Image round fit='cover' width='80px' height='80px' src='https://img.yzcdn.cn/vant/cat.jpeg' />
         <div className="flex flex-col items-start justify-between h-10 ml-3">
