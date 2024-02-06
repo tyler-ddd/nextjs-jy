72a9305159c197f496af261e64490eb0daee6086
温漂平 <wenpiaoping@192.168.0.101>
2024-02-07 00:28:10 +0800

    xifu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-web-app.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+    };
+  },[])
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
