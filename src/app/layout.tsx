196ec89d88a0addf6c5b1ef6b650c39590b550c8
温漂平 <wenpiaoping@192.168.0.101>
2024-02-07 23:56:44 +0800

    333

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { WebAppProvider } from '@vkruglikov/react-telegram-web-app';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,10 +18,8 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-      <WebAppProvider>
       <body className={inter.className}>
       {children}</body>
-      </WebAppProvider>
     </html>
   )
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -2,32 +2,27 @@
 import Link from "next/link"
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { BackButton, useWebApp } from '@vkruglikov/react-telegram-web-app';
+import { TelegramProvider, useTelegram } from "../../lib/TelegramProvider";
 import {Image,Divider } from 'react-vant';
 
 export default function User() {
-    const { back } = useRouter();
-    const { initWebAppData } = useWebApp();
-    useEffect(() => {
-        // 获取用户信息
-        const user = initWebAppData?.user;
-        console.log(user);
-      }, []);
+    const { user, webApp } = useTelegram();
+    console.log(user);
+   
   return (
     <div className='container p-4 pt-6'>
-        <BackButton onClick={back} />
         <div className="flex items-center justify-start w-full h-24">
         <Image round fit='cover' width='80px' height='80px' src='https://img.yzcdn.cn/vant/cat.jpeg' />
         <div className="flex flex-col items-start justify-between h-10 ml-3">
-            <h3 className="text-xl font-medium ">牛逼卡拉斯</h3>
-            <p className="text-sm ">ID：5564545</p>
+            <h3 className="text-xl font-medium ">{user?.first_name}</h3>
+            <p className="text-sm ">ID：{user?.id}</p>
         </div>
         </div>
         <Divider className="my-3 " />
         <div className="mt-4 ">
             <h1 className="w-full text-xl font-medium">个人资料</h1>
             <div className="flex flex-col text-sm">
-                <div className="mt-3 ">电报昵称：@uss 555</div>
+                <div className="mt-3 ">电报昵称：@{user?.username}</div>
                 <div className="flex mt-3"><span>预支详情：</span><span style={{'color':'red'}}>-20trx</span><span style={{'color':'red'}}>-20能量值</span></div>
                 <div className="mt-3 ">usdt余额：8888usdt</div>
                 <div className="mt-3 ">TRx余额：565trx</div>
